4af40ed993656d89d34f8d14c6c558ab1ffff519
1Juary <1457449065@qq.com>
2022-05-26 15:16:20 +0800

    增加自己实现插入排序（新建文件夹）

---
+++ Project1自己实现插入排序/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+typedef int ElemType;
+typedef struct {
+	ElemType* elem;                      //ÕûĞÍÖ¸Õë
+	int TableLen;
+}SSTable;
